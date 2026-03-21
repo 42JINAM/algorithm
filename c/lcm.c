a074da72433e9ccf870c00272e53815669393766
42JINAM <jinam@student.42.fr>
2026-03-21 18:18:05 +0100

    lcm

---
+++ c/lcm.c
@@ -0,0 +1,13 @@
+//Lcm is the least common multiple of both input numbers
+
+int	gcd(int a, int b)
+{
+	if (b == 0)
+		return (a);
+	return (gcd(b, a % b));
+}
+
+int	lcm(int a, int b)
+{
+	return (a / gcd(a, b) * b);
+}
