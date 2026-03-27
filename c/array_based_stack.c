b53a2044d7eba2d6816e833e0d4d72e6a7d0816c
42JINAM <jinam@student.42.fr>
2026-03-27 11:05:17 +0100

    Implement resizing for array based stack on shrink/expand

--- c/array_based_stack.c
+++ c/array_based_stack.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 typedef int ElementType ;
 
 typedef struct s_node
@@ -12,6 +13,12 @@ typedef	struct	s_arrayStack
 	int		top;
 	t_node	*nodes;
 }	t_arrayStack;
+
+void	AS_Destroy(t_arrayStack *stack)
+{
+	free(stack->nodes);
+	free(stack);
+}
 
 void	AS_CreateStack(t_arrayStack **stack, int capacity)
 {
@@ -32,12 +39,41 @@ void	AS_CreateStack(t_arrayStack **stack, int capacity)
 
 void	AS_Push(t_arrayStack *stack, ElementType data)
 {
+	if (stack->top + 1 == stack->capacity)
+	{
+		stack->capacity *= 1.3;
+		t_node *temp = (t_node *)malloc(sizeof(t_node) * stack->capacity);
+		if (temp == NULL)
+		{
+			AS_Destroy(stack);
+			exit(1);
+		}
+		memmove(temp, stack->nodes, sizeof(t_node) * (stack->top + 1));
+		free(stack->nodes);
+		stack->nodes = temp;
+	}
+
 	stack->top ++;
 	stack->nodes[stack->top].data = data;
 }
 
 ElementType	AS_Pop(t_arrayStack *stack)
 {
+	if (stack->top < stack->capacity * 0.7)
+	{
+		int new_cap = stack->capacity * 7 / 10;
+
+		if (new_cap < 10)
+			new_cap = 10;
+		t_node *temp = realloc(stack->nodes, new_cap * sizeof(t_node));
+		if (temp == NULL)
+		{
+			AS_Destroy(stack);
+			exit(1);
+		}
+		stack->nodes = temp;
+		stack->capacity = new_cap;
+	}
 	int	position = stack->top--;
 	return stack->nodes[position].data;
 }
@@ -52,11 +88,6 @@ int	AS_IsEmpty(const t_arrayStack *stack)
 	return(stack->top != -1);
 }
 
-void	AS_Destroy(t_arrayStack *stack)
-{
-	free(stack->nodes);
-	free(stack);
-}
 
 int		AS_GetSize(const t_arrayStack *stack)
 {
